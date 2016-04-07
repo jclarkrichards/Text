d93cfe606294e4bd153831de82fa496fff2dce53
jclarkrichards <jclarkrichards@gmail.com>
2016-04-07 13:54:59 -0400

    Update testrun.py

--- testrun.py
+++ testrun.py
@@ -8,9 +8,15 @@ screen = pygame.display.set_mode((500,500), 0, 32)
 background = pygame.surface.Surface((500,500)).convert()
 background.fill((0,0,0))
 
+txt = Text()
+phrase = PhraseHandler()
+phrase.setPhrase("Hello World!")
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
+            
+    phrase.print(screen)
     
     pygame.display.update()
