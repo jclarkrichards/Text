02fa890081b7e7c18bf765de77568250541b98b7
jclarkrichards <jclarkrichards@gmail.com>
2016-04-07 14:07:08 -0400

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -3,7 +3,15 @@ from base import Text
 class PhraseHandler(object):
     def __init__(self, phrase):
         self.phrase = phrase
+        self.phraseList = []
         
+    def setPhrase(self, lookup):
+        for letter in self.phrase:
+            try:
+                self.phraseList.append(lookup.textDict[letter])
+            except KeyError:
+                pass
+            
     def format(self):
         '''Format the phrase'''
         pass
