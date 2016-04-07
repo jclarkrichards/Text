2ffbe07d5c455c16c6cf118c2b791a32d71f9f12
jclarkrichards <jclarkrichards@gmail.com>
2016-04-07 13:58:01 -0400

    Update testrun.py

--- testrun.py
+++ testrun.py
@@ -8,9 +8,10 @@ screen = pygame.display.set_mode((500,500), 0, 32)
 background = pygame.surface.Surface((500,500)).convert()
 background.fill((0,0,0))
 
-txt = Text()
+txt = Text(spritesheet, 'text_map.txt', (16,16))
 phrase = PhraseHandler()
 phrase.setPhrase("Hello World!")
+phrase.format(position=(100,100))
 
 while True:
     for event in pygame.event.get():
