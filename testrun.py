4d71f28a2e71d198e48b2308f788ff15ac5f601b
jclarkrichards <jclarkrichards@gmail.com>
2016-04-07 12:27:05 -0400

    Update testrun.py

--- testrun.py
+++ testrun.py
@@ -5,7 +5,8 @@ from phrase import PhraseHandler
 
 pygame.init()
 screen = pygame.display.set_mode((500,500), 0, 32)
-
+background = pygame.surface.Surface((500,500)).convert()
+background.fill((0,0,0))
 
 while True:
     pass
