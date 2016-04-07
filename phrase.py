f199388eb8c3fc349a927e9cb94b7b0e02a47c0e
jclarkrichards <jclarkrichards@gmail.com>
2016-04-07 12:23:46 -0400

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -3,3 +3,11 @@ from base import Text
 class PhraseHandler(object):
     def __init__(self, phrase):
         self.phrase = phrase
+        
+    def format(self):
+        '''Format the phrase'''
+        pass
+    
+    def print(self, screen):
+        '''Print the phrase onto the screen'''
+        pass
