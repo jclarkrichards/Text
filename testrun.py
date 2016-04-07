2412a0804ce76b4c62081df854ee631b98365dfc
jclarkrichards <jclarkrichards@gmail.com>
2016-04-07 12:25:46 -0400

    Create testrun.py

---
+++ testrun.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame.locals import *
+from base import Text
+from phrase import PhraseHandler
+
+pygame.init()
+screen = pygame.display.set_mode((500,500), 0, 32)
+
+
+while True:
+    pass
