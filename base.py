1e533de5edb0533acc4050b2a8a3ea337deefb6f
jclarkrichards <jclarkrichards@gmail.com>
2016-04-08 10:01:54 -0400

    Update base.py

--- base.py
+++ base.py
@@ -1,40 +1,19 @@
 """Any class that uses text needs to inherit from this class"""
-import os
+import pygame
 from image_set import SpriteHandler
 from vectors import Vector2D
-#from entity import Character
 import numpy as np
 
 class Text(object):
     def __init__(self, sheet, txtfile, charsize):
-        self.sheet = sheet #'deluxe8bit.png'
+        self.sheet = sheet
         self.txtfile = txtfile
         self.charsize = charsize
         self.imageChars = []
         self.textChars = []
         self.textDict = {}
         self.loadSheet()
-        #self.linespace = 2
-        #self.wordspace = 8
-       
-        #self.position = Vector2D(position)
-        #self.area = Vector2D(area)
-        #self.charlist = []
-        #self.phraselist = []
-        #self.loadSheet(self.charsize)
-        #self.makeDict()
-        #self.phrase = phrase
-        #self.spaces = []
-        #self.textoffset = 0
-        #self.name = 'text'
-        
-    """
-    def setTextMap(self):
-        '''Open the txt file that maps the text to the image sheet'''
-        with open(self.txtfile) as f:
-            self.textChars = np.array(f.read().split(), dtype=str)
-        self.textChars = list(self.textChars)
-    """    
+    
     def getRowNums(self):
         rowNums = []
         self.textChars = []
@@ -59,84 +38,14 @@ class Text(object):
         if len(self.textChars) != len(self.imageChars):
             return "textChars and imageChars do not match!"
         for i, char in enumerate(self.textChars):
-            #self.textDict[char] = Character(self.imageChars[i])
             self.textDict[char] = self.imageChars[i]
-            
-            
-    #All of the methods below may be in the Phrase class        
-    def useUpperCase(self):
-        '''Use only uppercase letters'''
-        self.phrase = self.phrase.upper()
-
-    def setPhrase(self, phrase):
-        '''Set the phrase to print to screen'''
-        self.phrase = phrase
-
-    def updatePhrase(self, newphrase):
-        '''Update the phrase with a new phrase'''
-        self.phrase = str(newphrase)
-        self.parsePhrase()
-        self.interpret()
-
-    def interpret(self):
-        '''Interpret a string into their corresponding character images.
-        Determine the x,y position of each character.'''
-        self.charlist = []
-        maxrows = int(self.area.y / (self.charsize[1]+self.linespace))
-        if maxrows < 1:
-            maxrows = 1
-        w, h = self.charsize
-        row = 0
-        while row < maxrows:
-            try:
-                thisphrase = self.phraselist[0]
-            except IndexError:
-                row = maxrows
-            else:
-                for j in range(len(thisphrase)):
-                    pos = (j*w+self.position.x+self.textoffset,
-                           row*(self.linespace+h)+self.position.y)
-                    try:
-                        image = self.textdict[thisphrase[j]]
-                    except:
-                        self.spaces.append((pos[0]-self.position.x) / w)
-                    else:
-                        self.charlist.append(AbstractEntity((w,h),image=image,
-                                                            startpos=pos))
-                self.phraselist.remove(thisphrase)
-            row += 1
-
-    def parsePhrase(self):
-        '''Split the phrase into multiple lines if it exceeds MAXWIDTH.'''
-        dx,dy = self.charsize
-        width, height = self.area.toTuple()
-        wordlist = self.phrase.split(' ')
-        while len(wordlist) > 0:
-            n = 0
-            templist = []
-            nextline = False
-            while not nextline and len(wordlist) > 0:
-                word = wordlist[0]
-                n += len(word)*dx
-                if n > width:
-                    nextline = True
-                else:
-                    templist.append(word)
-                    wordlist.remove(word)
-                    n += self.wordspace
-            self.phraselist.append(" ".join(templist))
-
-    def fromTextFile(self, filename, thisline):
-        '''Get a phrase from a text file.  Assume that each phrase
-        is on a different line in the file'''
-        indx = 0
-        with open(filename, "r") as f:
-            for line in f:
-                if indx == thisline:
-                    self.phrase = line
-                    self.phrase.strip('\n')
-                    break
-                indx += 1
+        self.addSpaceCharacter()
+        
+    def addSpaceCharacter(self):
+        '''Add a space character to the dictionary'''
+        self.textDict[' '] = pygame.Surface(self.charsize)
+        
+   
 
 
 
