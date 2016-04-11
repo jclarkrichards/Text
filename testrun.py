ee9821baf745c16163330dd27e3722a53272b6ca
jclarkrichards <jclarkrichards@gmail.com>
2016-04-11 10:02:08 -0400

    Update testrun.py

--- testrun.py
+++ testrun.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from base import Text
 from phrase import PhraseHandler
+from textboxes import TextBox
 
 pygame.init()
 screen = pygame.display.set_mode((500,500), 0, 32)
@@ -9,20 +10,26 @@ background = pygame.surface.Surface((500,500)).convert()
 background.fill((0,0,0))
 clock = pygame.time.Clock()
 txt = Text('deluxefont8px.png', 'text_map.txt', (8,8))
-phrase = PhraseHandler("Jonathan Richards")
-phrase.mapPhrase(txt.textDict)
+
+box = TextBox(3, 20)
+box.setPosition((10,200))
+box.setPhrase("Jonathan Richards")
+
+#phrase = PhraseHandler("Jonathan Richards")
+#phrase.mapPhrase(txt.textDict)
 #phrase.setScale(3)
-phrase.setPosition((10,200))
-phrase.readoutCharacters(15)
+#phrase.setPosition((10,200))
+#phrase.readoutCharacters(15)
 
 while True:
     dt = clock.tick(30)/1000.0
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
-
-    phrase.update(dt)
+    box.update(dt)
+    #phrase.update(dt)
     screen.blit(background, (0,0))
-    phrase.render(screen)
+    #phrase.render(screen)
+    box.render(screen)
     
     pygame.display.update()
