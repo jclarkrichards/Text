ee4e4f87f1bac82ce91b8e9e54f7c62a176f3576
jclarkrichards <jclarkrichards@gmail.com>
2016-04-11 09:54:33 -0400

    Update textboxes.py

--- textboxes.py
+++ textboxes.py
@@ -1,4 +1,16 @@
+from vectors import Vector2D
 
 class TextBox(object):
-    def __init__(self, phrase):
-        pass
+    def __init__(self, lines, charsPerLine):
+        self.charPerLine = charsPerLine
+        self.lines = lines
+        self.position = Vector2D()
+        self.phrase = ''
+        
+    def setPosition(self, position):
+        self.position = Vector2D(position)
+        
+    def setPhrase(self, phrase):
+        '''The phrase is a string'''
+        self.phrase = phrase
+        
