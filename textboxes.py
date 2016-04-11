b184c129692e800a9f90b2f1a6ca129c9d2fca9c
jclarkrichards <jclarkrichards@gmail.com>
2016-04-11 10:36:41 -0400

    Update textboxes.py

--- textboxes.py
+++ textboxes.py
@@ -8,7 +8,7 @@ class TextBox(object):
         self.position = Vector2D()
         self.width = 0
         self.height = 0
-        self.phrase = ''
+        self.phrase = None
         self.charsize = (0, 0)
         
     def update(self, dt):
@@ -16,20 +16,23 @@ class TextBox(object):
     
     def setDimensions(self):
         '''dimensions based on characters width and height'''
-        pass
+        self.width = self.charPerLine*self.charsize[0]
+        self.height = self.lines*self.charsize[1]
     
     def setPosition(self, position):
         self.position = Vector2D(position)
         
     def setPhrase(self, phrase, table):
-        '''The phrase is a string. table maps the characters'''
+        '''The input phrase is a string. table is the dictionary'''
         self.phrase = PhraseHandler(phrase)
-        self.phrase.mapPhrase(table)
-        self.charsize = self.phrase[0].get_size()
+        self.phrase.mapPhrase(table) #self.phrase.phraseList
+        self.charsize = self.phrase[0].size()
+        self.setDimensions()
         
     def scaleCharacters(self, scale):
         '''Set dimensions of characters.  Changes dimensions of box'''
         self.phrase.setScale(scale)
+        self.charsize = self.phrase[0].size()
         self.setDimensions()
         
     def render(self, screen):
