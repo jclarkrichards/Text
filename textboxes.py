81b1f0535810ecccfbaf4fed90db247e99d6e2f4
jclarkrichards <jclarkrichards@gmail.com>
2016-04-12 14:04:30 -0400

    Update textboxes.py

--- textboxes.py
+++ textboxes.py
@@ -51,6 +51,7 @@ class TextBox(object):
         index = 0
         offsetX = 0 #0 for first word, 1 otherwise
         offsetY = 0 #1 for first word, 0 otherwise
+        charlist = []
         
         for iword, word in enumerate(words):
             if numChars+len(word)+offsetX <= self.charPerLine:
@@ -60,14 +61,20 @@ class TextBox(object):
                 line += 1
                 offsetY = 1
                 col = 0
+                if line >= self.lines:
+                    line = 0
+                    self.phrase.phraseArray.append(charlist)
+                    charlist = []
 
             for i in range(index+offsetY, len(word)+index+offsetX):
                 self.phrase.phraseList[i].setPosition(self.position, col, line)
+                charlist.append(self.phrase.phraseList[i])
                 col += 1
                 
             offsetX = 1
             offsetY = 0
             index = i+1
+        self.phrase.phraseList = self.phrase.phraseArray[0]
 
     def readoutCharacters(self, speed):
         self.phrase.readoutCharacters(speed)
