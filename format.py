b4f10db038e4832086041158e90b7293baa00481
jclarkrichards <jclarkrichards@gmail.com>
2016-04-13 13:35:14 -0400

    Update format.py

--- format.py
+++ format.py
@@ -1,19 +1,22 @@
 """Left justify text"""
 
 class LeftJust(object):
-    def __init__(self):
-        pass
+    def __init__(self, phrase=''):
+        self.phrase = phrase
+        self.words = phrase.split()
+        self.line = 0
+        self.col = 0
+        self.numChars = 0
+        self.index = 0
+        self.offsetX = 0 #0 for 1st word in phrase, 1 otherwise
+        self.offsetY = 0 #1 for 1st word in line, 0 otherwise
+        self.charlist = []
     
+    def setFormat(self):
+        for iword, word in enumerate(self.words):
+            pass
     '''
-    words = phrase.split()
-        line = 0
-        col = 0
-        numChars = 0
-        index = 0
-        offsetX = 0 #0 for first word, 1 otherwise
-        offsetY = 0 #1 for first word, 0 otherwise
-        charlist = []
-        
+    
         for iword, word in enumerate(words):
             if numChars+len(word)+offsetX <= self.charPerLine:
                 numChars += len(word)+offsetX
